0e5da9f726dc41af3dd95f0eee75f3be24ec848d
Brian Hoy <brianhoy4@gmail.com>
2020-11-22 21:01:44 -0500

    initial commit

--- glsl-asset.js
+++ glsl-asset.js
@@ -1,4 +1,7 @@
 const { Asset } = require('parcel-bundler');
+const fs = require('fs');
+const path = require('path');
+const { exception } = require('console');
 
 module.exports = class GLSLAsset extends Asset {
 
@@ -7,8 +10,30 @@ module.exports = class GLSLAsset extends Asset {
     this.type = 'js';
   }
 
+  replaceIncludes(text, depth = 0) {
+    if(depth > 50) {
+      throw "parcel-plugin-glsl failed to compile due to circular dependency";
+    }
+
+    let dirname = path.dirname(this.name);
+    let regex = /^\s*#include\s"(.*)"\s*$/gm;
+    var ref = this;
+    let result = text.replace(regex, function(match, fileStr) {
+      let file = path.resolve(dirname, fileStr);
+      ref.addDependency(file, {includedInParent: true});
+      let includedText = fs.readFileSync(file, "utf8");
+      return ref.replaceIncludes(includedText, depth + 1);
+    });
+    return result + "\n";
+  }
+
   generate() {
-    return { js: `module.exports = \`${ this.contents }\`` };
+    // this.invalidate();
+    let text = this.contents;
+
+    text = this.replaceIncludes(text)
+
+    return { js: `module.exports = \`${ text }\`` };
   }
 
 };
